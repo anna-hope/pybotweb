5646bf2755f73b302343b1952dfa69a7341c26b9
Anton Osten <anton@ostensible.me>
2015-03-02 22:39:02 -0600

    removed config from git

--- config.py
+++
@@ -1,8 +0,0 @@
-class PybotConfig:
-	pass
-
-class DebugConfig(PybotConfig):
-	'SQLALCHEMY_DATABASE_URI' = 'sqlite:////tmp/test.db'
-
-class ProductionConfig(PybotConfig):
-	'SQLALCHEMY_DATABASE_URI' = 'sqlite:////pybot.db'
