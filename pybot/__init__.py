d0e279035d5021f8c524efeba18c6009ec53fc0a
Anton Osten <anton@ostensible.me>
2015-03-02 22:39:13 -0600

    login manager for the app

--- pybot/__init__.py
+++ pybot/__init__.py
@@ -1,10 +1,15 @@
 import pathlib, importlib
 
+from flask.ext.login import LoginManager
+
 from flask import Flask
 
 app = Flask('pybot')
+login_manager = LoginManager()
 
 def load_app():
+	login_manager.init_app(app)
+
 	importlib.import_module('{}.views'.format(app.name))
 	views = pathlib.Path(app.root_path, 'views')
 	for file in views.iterdir():
