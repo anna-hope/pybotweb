5250347068d567736780cc7928c2e937b2729ea1
Anton Osten <anton@ostensible.me>
2015-03-07 20:21:09 -0600

    compile coffeescript when run

--- runme.py
+++ runme.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python3.4
 
-from pybot import app, load_app
-import click
+import pathlib
 
+from pybot import app, load_app
+import click, coffeescript
+
+
+def compile_coffee(coffee_path='scripts', output_path=('static', 'scripts')):
+    path = pathlib.Path(app.root_path, coffee_path)
+    out_path = pathlib.Path(app.root_path, *output_path)
+    if not out_path.exists():
+        out_path.mkdir()
+
+    for file in path.iterdir():
+        if file.suffix == ('.coffee'):
+            out_file = pathlib.Path(out_path, (file.stem + '.js'))
+
+            with file.open() as coffeefile:
+                js_text = coffeescript.compile(coffeefile.read())
+            with out_file.open('w') as outfile:
+                outfile.write(js_text)
 
 def run_production(host, port):
     load_app()
     # fix to use a production server
     app.run(host=host, port=port)
 
-def run_debug(host, port):
+def run_debug(host, port, straightcoffee=False):
     load_app()
     app.config.from_object('config.DebugConfig')
+
+    if straightcoffee:
+        # code to run coffeescript straight
+        ...
+    else:
+        # compile coffeescript
+        compile_coffee()
+        app.config['STRAIGHTCOFFEE'] = True
+
     app.run(debug=True, host=host, port=port)
 
 @click.command()
 @click.option('--host', default='localhost', help='host')
 @click.option('--port', default=8000, help='port')
 @click.option('--debug', is_flag=True)
-def run(host, port, debug):
+@click.option('--straightcoffee', is_flag=True)
+def run(host, port, debug, straightcoffee):
     if debug:
-        run_debug(host, port)
+        run_debug(host, port, straightcoffee)
     else:
         run_production(host, port)
 
