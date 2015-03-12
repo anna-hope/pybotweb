657979f12313cf5a9c6a4dcc264fa80bc2b14db6
Anton Osten <anton@ostensible.me>
2015-03-11 19:35:58 -0500

    better styling of output

--- pybot/static/scripts/prepare_play.py
+++ pybot/static/scripts/prepare_play.py
@@ -1,11 +1,19 @@
 import sys
-from browser import document
+from browser import document, html
 
 class ElementWriter:
 	def __init__(self, dom_element_id):
 		self.element = document[dom_element_id]
 
 	def write(self, string):
-		self.element <= string
+		output_element = html.P(string)
+		self.element <= output_element
+
+class ErrorWriter(ElementWriter):
+	def write(self, string):
+		error_div = html.DIV(string, id='error_output',
+							 style={'color': 'red'})
+		self.element <= error_div
 
 sys.stdout = ElementWriter('output_area')
+sys.stderr = ErrorWriter('output_area')
