065cb43a0db5dede63480c3093f1140a065d9c7f
Anton Osten <anton@ostensible.me>
2015-03-20 20:14:27 -0500

    marked as linkable endpoint

--- pybot/views/play_code.py
+++ pybot/views/play_code.py
@@ -1,6 +1,7 @@
-from pybot import app
+from pybot import app, helpers
 from flask import render_template
 
 @app.route('/play/')
+@helpers.linkable
 def play():
 	return render_template('play.html')
