f3f64d11611147658450e8f7242c7b2cff314e18
Anton Osten <anton@ostensible.me>
2015-03-20 20:11:22 -0500

    decorator for marking linkable endpoint functions

--- pybot/helpers.py
+++ pybot/helpers.py
@@ -1,6 +1,18 @@
+import functools
+
+from pybot import app
+
 from flask import jsonify
 
 def make_json_message(status: str, message: str, **kwargs) -> 'json':
 	message = {'status': status, 'message': message}
 	message.update(kwargs)
 	return jsonify(message)
+
+def linkable(func):
+	try:
+		app.config['LINKABLE_ENDPOINTS'].append(func.__qualname__)
+	except KeyError:
+		app.config['LINKABLE_ENDPOINTS'] = []
+		app.config['LINKABLE_ENDPOINTS'].append(func.__qualname__)
+	return func
