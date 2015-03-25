32462abf949f68d0b9eedadbe3224af47c796fd8
Anton Osten <anton@ostensible.me>
2015-03-25 15:12:19 -0500

    slugify + fixes

--- pybot/helpers.py
+++ pybot/helpers.py
@@ -1,8 +1,11 @@
-import functools
-
 from pybot import app
+from pybot.db import dbhelpers
 
 from flask import jsonify
+from slugify import Slugify
+
+_slugify = UniqueSlugify()
+_slugify.uids = set(dbhelpers.get_page_slugs())
 
 def make_json_message(status: str, message: str, **kwargs) -> 'json':
 	message = {'status': status, 'message': message}
@@ -13,6 +16,8 @@ def linkable(func):
 	try:
 		app.config['LINKABLE_ENDPOINTS'].append(func.__qualname__)
 	except KeyError:
-		app.config['LINKABLE_ENDPOINTS'] = []
-		app.config['LINKABLE_ENDPOINTS'].append(func.__qualname__)
+		app.config['LINKABLE_ENDPOINTS'] = [func.__qualname__]
 	return func
+
+def slugify(title: str) -> str:
+	return _slugify(title)
