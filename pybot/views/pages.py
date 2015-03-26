52ec6e0b79b756d3d07be7fa94257da8190e10d9
Anton Osten <anton@ostensible.me>
2015-03-26 16:35:59 -0500

    add new page

--- pybot/views/pages.py
+++ pybot/views/pages.py
@@ -1,7 +1,16 @@
 from pybot import app
+from pybot import helpers
 from pybot.db import dbhelpers
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
+from wtforms import Form, StringField, TextAreaField, SelectField, validators
+
+class NewPageForm(Form):
+	page_title = StringField('title', (validators.required(), 
+										validators.length(max=50)))
+	page_category = SelectField('category', choices=[('new', 'new')])
+	new_page_category = StringField('new category', (validators.length(max=50),))
+	new_page_content = TextAreaField('content (markdown)', (validators.required(),))
 
 
 def get_page(slug: str):
@@ -11,16 +20,16 @@ def get_category(slug: str):
 	return dbhelpers.get_page_category(slug)
 
 def get_categories():
-	return dbhelpers.get_all_page_categories():
+	return dbhelpers.get_all_page_categories()
 
 @app.route('/pages/')
 @app.route('/pages/<slug>/')
-@linkable
+@helpers.linkable
 def render_page(slug=None):
 	if slug:
 		page = get_page(slug)
 		if page:
-			return render_template('pages.html', page=page, category=False)
+			return render_template('pages.html', page=page, mode='page')
 		else:
 
 			# try finding a page category
@@ -32,11 +41,43 @@ def render_page(slug=None):
 											 'slug': p.slug}
 											 for p in category.pages.all()]})
 			else:
-				return render_template('pages.html', page=(category,), category=True)
+				return render_template('pages.html', page=(category,), mode='category')
 	else:
-		return render_template('pages.html', page=dbhelpers.get_categories(),
-								 category=True)
+		return render_template('pages.html', page=get_categories(),
+								 mode='category')
 
 @app.route('/pages/<category>/<slug>/')
 def render_subpage(category, slug):
 	...
+
+@app.route('/pages/add_new/', methods=('GET', 'POST'))
+def add_new_page():
+	if request.method == 'GET':
+		new_page_form = NewPageForm()
+		new_page_form.page_category.choices += [(c.slug, c.title) for c in get_categories()]
+		return render_template('pages.html', mode='new_page', form=new_page_form)
+
+	else:
+		new_page_form = NewPageForm(request.post)
+		new_page_form.page_category.choices += [(c.slug, c.title) for c in get_categories()]
+
+		if new_page_form.validate():
+			title = new_page_form.page_title.data
+			content = new_page_form.new_page_content.data
+			category = new_page_form.page_category.data
+
+			if category == 'new':
+				category = new_page_form.new_page_category.data
+
+			dbhelpers.create_page(title, content, category)
+
+			if request.args.get('asjson'):
+				return helpers.make_json_message(
+					'success', 'page {} was added'.format(title))
+			return 'success'
+
+		else:
+			if request.args.get('asjson'):
+				return helpers.make_json_message(
+					'error', new_page_form.errors)
+			return 'success'
