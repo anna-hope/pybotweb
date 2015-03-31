9b5cc3d8bffb12aa1d42e1174e771aa563615685
Anton Osten <anton@ostensible.me>
2015-03-31 16:46:05 -0500

    decorator for marking the login view

--- pybot/helpers.py
+++ pybot/helpers.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from pybot import app
+from pybot import app, login_manager
 from pybot.db import dbhelpers
 
 from flask import jsonify
@@ -9,6 +9,8 @@ import mistune
 
 _slugify = UniqueSlugify(to_lower=True)
 result_tuple = namedtuple('Result', ('status', 'message'))
+
+# shortcuts to send response messages
 
 def make_json_message(status: str, message: str, **kwargs) -> 'json':
 	message = {'status': status, 'message': message}
@@ -25,11 +27,17 @@ def get_result_message(pred: bool, success_msg: str, failure_msg: str,
 def form_error_message(form):
 	return make_json_message('error', 'invalid form data', errors=form.errors)
 
+# view decoratos
+
 def linkable(func):
 	try:
 		app.config['LINKABLE_ENDPOINTS'].append(func.__qualname__)
 	except KeyError:
 		app.config['LINKABLE_ENDPOINTS'] = [func.__qualname__]
+	return func
+
+def login_view(func):
+	login_manager.login_view = func.__qualname__
 	return func
 
 def slugify(title: str) -> str:
