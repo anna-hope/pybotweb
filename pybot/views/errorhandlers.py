6de77e01a21175910b7e52856fb115993acede9e
Anton Osten <anton@ostensible.me>
2015-04-27 18:36:27 -0500

    error 500 handler

--- pybot/views/errorhandlers.py
+++ pybot/views/errorhandlers.py
@@ -9,3 +9,7 @@ def not_found(error):
 @app.errorhandler(401)
 def unauthorized(error):
 	return render_template('401.html'), 401
+
+@app.errorhandler(500)
+def server_error(error):
+	return render_template('500.html'), 500
